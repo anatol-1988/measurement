ee1ddf679e3729d992395a0ae87dda4bc7bb0423
anatol <anatol1988@gmail.com>
2019-12-15 14:27:08 +0300

    Add an example

---
+++ tests/Main.elm
@@ -0,0 +1,53 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Measurement as GA
+
+
+type alias Model =
+    { text : String }
+
+
+init : () -> ( Model, Cmd msg )
+init flags =
+    ( { text = "Model" }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+type Msg
+    = SendEvent
+    | Ga GA.Msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SendEvent ->
+            ( model, Cmd.map Ga <| GA.pageview "UA-151596008-1" "555" "/home" )
+
+        Ga m ->
+            let _ = (Debug.log "message" m)
+            in ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick SendEvent ] [ text "Send Event" ]
+        ]
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , subscriptions = subscriptions
+        , view = view
+        , update = update
+        }
