e17ca58cfc3fa632faaac8752e312e98f8c14882
anatol <anatol1988@gmail.com>
2020-01-16 23:18:32 +0300

    Add docs

--- src/Measurement.elm
+++ src/Measurement.elm
@@ -1,4 +1,22 @@
-module Measurement exposing (Msg, event, get, pageview, post)
+module Measurement exposing
+    ( get, post, batch
+    , event, pageview, Parameter
+    )
+
+{-| This module implements Google protocol for Analytics. You can find its
+description on <https://developers.google.com/analytics/devguides/collection/protocol/v1>
+
+
+# Request constructors
+
+@docs get, post, batch, Parameter
+
+
+# Helpers
+
+@docs event, pageview
+
+-}
 
 import HitType exposing (HitType)
 import Http
@@ -9,15 +27,36 @@ import Url.Builder as Url
 type Msg
     = Measured (Result Http.Error ())
 
+{-| Measurement Protocol Parameter
+-}
+type alias Parameter =
+    Parameter.Parameter
 
 type alias Hit =
     { hitType : HitType
     , trackingId : String
     , clientId : String
-    , payload : List Parameter.Parameter
+    , payload : List Parameter
     }
 
 
+{-| Sends hit to GA using POST GET request
+
+    get
+        { hitType = HitType.Event
+        , trackingId = "UA-XXXXX-Y"
+        , clientId = "555"
+        , payload =
+            [ Parameter.CacheBuster "289372387623"
+            , Parameter.EventCategory "video"
+            , Parameter.EventAction "play"
+            , Parameter.EventLabel "holiday"
+            , Parameter.EventValue 300
+            ]
+        }
+        |> Http.get
+
+-}
 get : Hit -> { expect : Http.Expect Msg, url : String }
 get hit =
     let
@@ -29,6 +68,22 @@ get hit =
     }
 
 
+{-| Sends hit to GA using POST HTTP request
+
+    post
+        { hitType = HitType.Event
+        , trackingId = "UA-XXXXX-Y"
+        , clientId = "555"
+        , payload =
+            [ Parameter.EventCategory "video"
+            , Parameter.EventAction "play"
+            , Parameter.EventLabel "holiday"
+            , Parameter.EventValue 300
+            ]
+        }
+        |> Http.post
+
+-}
 post : Hit -> { url : String, body : Http.Body, expect : Http.Expect Msg }
 post hit =
     { url = "https://www.google-analytics.com/collect"
@@ -53,6 +108,27 @@ payloadToQuery hit =
     String.dropLeft 1 <| Url.toQuery payload
 
 
+{-| Batching multiple hits in a single request
+    batch
+      [ { hitType = HitType.Event
+        , trackingId = "UA-XXXXX-Y"
+        , clientId = "555"
+        , payload =
+            [ Parameter.CacheBuster "289372387623"
+            , Parameter.EventCategory "video"
+            , Parameter.EventAction "play"
+            , Parameter.EventLabel "holiday"
+            , Parameter.EventValue 300
+            ]
+        }
+        , { hitType = HitType.Pageview
+          , trackingId = "UA-123456-1"
+          , clientId = "5555"
+          , payload = [ Parameter.DocumentPath "/pageA" ]
+        }
+      ]
+      |> Http.post
+-}
 batch :
     List Hit
     ->
@@ -74,6 +150,12 @@ batch hits =
     }
 
 
+{-| Sends hit to GA HTTP post request with pageview hit. A pageview is an
+instance of a page being loaded (or reloaded) in a browser.
+
+    pageview "UA-XXXXX-Y" "555" "/pageA"
+
+-}
 pageview : String -> String -> String -> Cmd Msg
 pageview trackingId clientId documentPath =
     post
@@ -85,6 +167,14 @@ pageview trackingId clientId documentPath =
         |> Http.post
 
 
+{-| Sends hit to GA HTTP post request with event hit. Events are user
+interactions with content that can be measured independently from a web-page or
+screen load. Downloads, link clicks, form submissions, and video plays are all
+examples of actions you might want to analyze as Events.
+
+    event "UA-XXXXX-Y" "555" "Category" "Action" "Label" 55
+
+-}
 event : String -> String -> String -> String -> String -> Int -> Cmd Msg
 event trackingId clientId category action label value =
     post
